cb2bd2cc3fbc343fed7d2dd457c350702c010b37
Joseph Watkins <josephxwatkins@gmail.com>
2022-07-27 16:04:14 -0400

    first 5 tests passing

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function trimProperties(obj) {
   for (let prop in obj) {
     result[prop] = obj[prop].trim()
   }
-  return result
+  return result;
 }
 
 /**
@@ -25,6 +25,10 @@ function trimProperties(obj) {
  */
 function trimPropertiesMutation(obj) {
   // ✨ implement
+  for (let prop in obj) {
+    obj[prop] = obj[prop].trim()
+  }
+  return obj;
 }
 
 /**
@@ -37,6 +41,13 @@ function trimPropertiesMutation(obj) {
  */
 function findLargestInteger(integers) {
   // ✨ implement
+  let result = integers[0].integer
+  for (let i = 0; i < integers.length; i++) {
+    if (integers[i].integer > result) {
+      result = integers[i].integer
+    }
+  }
+  return result
 }
 
 class Counter {
